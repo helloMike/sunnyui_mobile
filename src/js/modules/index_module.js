06e1b687941acfdc6af790fe2aea091400dc0903
yxzone <xiaoxing200888@gmail.com>
2016-03-03 17:25:10 +0800

    20160303 17:24 星期四 coorperation.html基本完成

--- src/js/modules/index_module.js
+++ src/js/modules/index_module.js
@@ -4,12 +4,14 @@
 define(function(){
     var DEFAULT_WIDTH = 320,// 画布的默认宽度
         ua = navigator.userAgent.toLowerCase(); // 根据 user agent 的信息获取浏览器信息
+    //设定安卓机的viewport的宽度
     var android_viewport = function(){
         if (ua.indexOf("android") !== -1 || 1){
             $('meta[name="viewport"]').attr('content',
                 'width='+ DEFAULT_WIDTH+'px');
         }
     }
+
     var press = function() {
         $('.main_index li').hammer().bind('press tap', function () {
             $(this).css("backgroundColor", "rgba(0,0,0,0.5)")
